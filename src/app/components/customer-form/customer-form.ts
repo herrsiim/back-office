2b2b63a4321f0b948245d2417a3144a1e8fda8f0
Herrsiim <herrsiim@gmail.com>
2025-07-10 20:45:09 +0300

    add customer form component with validation and styling; include logo and update layout

--- src/app/components/customer-form/customer-form.ts
+++ src/app/components/customer-form/customer-form.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-customer-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './customer-form.html',
-  styleUrl: './customer-form.scss'
+  styleUrl: './customer-form.scss',
 })
 export class CustomerForm {
+  issuingCountries = ['EE', 'FI', 'LV', 'LT'];
+  birthCountries = ['Estonia', 'Finland', 'Latvia', 'Lithuania'];
+  form;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      idCode: ['', Validators.required],
+      idCountry: ['EE', Validators.required],
+      birthDate: [''],
+      birthCountry: [''],
+      email: [''],
+      emailVerified: [false],
+    });
+  }
+
+  get isEstonianId(): boolean {
+    return this.form.get('idCountry')?.value === 'EE';
+  }
+
+  verifyEmail() {
+    this.form.patchValue({ emailVerified: true });
+  }
+
+  unverifyEmail() {
+    this.form.patchValue({ emailVerified: false });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
